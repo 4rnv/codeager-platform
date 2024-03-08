a039f99939578d2ed0e38cf8005ee58e8e963a41
4rnv <arnavsdandekar@gmail.com>
2024-03-08 14:35:18 +0530

    "Added quotes+images"

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 
 #Use tailwind CDN for the css stuff
-#Add image and quote for each question, mayb use tuple
 
 app = Flask(__name__)
 
@@ -17,6 +16,15 @@ questions = ["What is the capital of France?",
 
 answers=["Paris", "4", "Blue whale", "Mars", "Au", "Harper Lee", "Diamond", "Rome"]
 
+quote_img = [('\"A is A, but sometimes, A is G.\"','1.png'),
+             ('\"Late late says the White Rabbit.\"','2.png'),
+             ('Somewhere, Under a Starry Sky.','3.png'),
+             ('Old Home','4.png'),
+             ('The Leaning Tower of Babel','5.png'),
+             ('Arrakis','6.png'),
+             ('...','7.png'),
+             ('Decretum','8.png')]
+
 q_no = 1
 
 @app.route("/")
@@ -26,9 +34,11 @@ def index():
 @app.route("/chapter/<q_no>/")
 def q(q_no):
    q_no = int(q_no)
+   array_index = int(q_no) -1
+   quote_ani_image = quote_img[array_index]
+   x = '/static/images/'
    if q_no >= 0:
-      array_index = int(q_no) -1
-      return render_template('q.html', question= questions[array_index], form_link = q_no)
+      return render_template('q.html', episode=q_no, question= questions[array_index], form_link = q_no, quote = quote_ani_image[0], image= x+quote_ani_image[1])
    else:
       return redirect("/", code=302)
 
@@ -42,11 +52,11 @@ def check_ans(q_no):
    user_answer= request.form['answer'].strip()
    if (user_answer.lower() == answers[array_index].lower()):
       if array_index >= 7:
-         return render_template('result.html', bane = 'Congratulations! You have completed the challenge', cia = 0, link_msg = 'Go home')
+         return render_template('result.html', ans_feedback = 'Congratulations! You have completed the challenge', question_pointer = 0, link_msg = 'Go home')
       else:
-        return render_template('result.html', bane = 'Congratulations! That is the correct answer', cia = int(q_no)+1, link_msg = 'Continue your story')
+        return render_template('result.html', ans_feedback = 'Congratulations! That is the correct answer', question_pointer = int(q_no)+1, link_msg = 'Continue your story')
    else:
-      return render_template('result.html', bane = 'Too bad! You\'ve failed', cia = int(q_no), link_msg = 'Try again')
+      return render_template('result.html', ans_feedback = 'Too bad! You\'ve failed', question_pointer = int(q_no), link_msg = 'Try again')
 
 @app.route('/credits/')
 def credits():
